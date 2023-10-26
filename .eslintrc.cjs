ed865c229426601e01b0834c3e3a0f1589e7bb05
sunhx <673686754@qq.com>
2023-10-26 20:44:18 +0800

    style: eslint and prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,14 +5,78 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    // prettier
+    // https://github.com/prettier/eslint-config-prettier#readme
+    'prettier',
+    // Runs Prettier as an ESLint rule and reports differences as individual ESLint issues
+    // https://github.com/prettier/eslint-plugin-prettier
+    'plugin:prettier/recommended'
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: [
+    'react-refresh',
+    'prettier',
+    'import',
+    'unused-imports'
+  ],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+
+    /* ********************************** Module Import ********************************** */
+    'import/no-absolute-path': 0,
+    'import/extensions': 0,
+    'import/no-named-default': 0,
+    'no-restricted-exports': 0,
+
+    // Forbid the use of extraneous packages
+    'import/no-extraneous-dependencies': [
+      1,
+      {
+        devDependencies: [
+          '**/*.test.{ts,js}',
+          '**/*.spec.{ts,js}',
+          './test/**.{ts,js}',
+          './scripts/**/*.{ts,js}',
+        ],
+      },
+    ],
+    // Enforce a convention in module import order
+    'import/order': [
+      1,
+      {
+        pathGroups: [
+          {
+            pattern: '@/**',
+            group: 'external',
+            position: 'after',
+          },
+        ],
+        alphabetize: { order: 'asc', caseInsensitive: false },
+        'newlines-between': 'always-and-inside-groups',
+        warnOnUnassignedImports: true,
+      },
+    ],
+    'sort-imports': [
+      'error',
+      {
+        ignoreCase: true,
+        ignoreDeclarationSort: true,
+      },
+    ],
+    // Find and remove unused es6 module imports
+    // https://github.com/sweepline/eslint-plugin-unused-imports
+    'unused-imports/no-unused-imports': 0,
+    'unused-imports/no-unused-vars': [
+      'error',
+      {
+        vars: 'all',
+        args: 'none',
+        ignoreRestSiblings: true,
+      },
+    ]
   },
 }
