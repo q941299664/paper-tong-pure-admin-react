40b4fc6c9a738fab951a990f786bc895e9a7026e
sunhx <673686754@qq.com>
2023-11-20 21:26:10 +0800

    feat: add back navigation to HeaderTitle component

--- src/components/layout/header/HeaderTitle.tsx
+++ src/components/layout/header/HeaderTitle.tsx
@@ -1,20 +1,29 @@
 import { LeftOutlined } from '@ant-design/icons'
-import { useLocation } from 'react-router-dom'
+import { useLocation, useNavigate } from 'react-router-dom'
 
 import { authRoutes } from '@/router/routes'
 import { searchRoute } from '@/utils'
 
 function HeaderTitle() {
   const { pathname } = useLocation()
+  const navigate = useNavigate()
+
   const isHomePage = ['home', 'dashboard-home'].includes(pathname)
   const canBack = !/.+\/list$/.test(pathname)
   const currentRoute = searchRoute(pathname, authRoutes)
+
+  const back = () => {
+    if (canBack) {
+      navigate(-1)
+    }
+  }
+
   return (
     !isHomePage && (
       <div className="h-[32px] flex justify-center items-center">
         {canBack ? (
           <span className="self-stretch w-[32px] text-xl rounded select-none cursor-pointer hover:bg-gray-100 hover:text-gray-900 flex items-center justify-center">
-            <LeftOutlined />
+            <LeftOutlined onClick={() => back()} />
           </span>
         ) : (
           <span className="self-stretch w-[32px] text-xl rounded select-none cursor-pointer flex items-center justify-center">
