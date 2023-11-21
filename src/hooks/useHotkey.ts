9cdae6445dab345f56ecf072a4edb73c46ced520
sunhx <673686754@qq.com>
2023-11-21 20:33:44 +0800

    feat: add useHotkey hook

---
+++ src/hooks/useHotkey.ts
@@ -0,0 +1,17 @@
+import hotkeys from 'hotkeys-js'
+import { useCallback, useEffect } from 'react'
+
+interface HotkeyParams {
+  hotkey: string
+  callback: () => void
+}
+
+export default function useHotkey({ hotkey, callback }: HotkeyParams) {
+  const cb = useCallback(callback, [callback])
+  useEffect(() => {
+    hotkeys(hotkey, cb)
+    return () => {
+      hotkeys.unbind(hotkey, cb)
+    }
+  }, [hotkey, cb])
+}
