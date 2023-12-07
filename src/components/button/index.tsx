6ce0b01b145f71f2cd905b6e0565b61be2eda6b4
sunhx <673686754@qq.com>
2023-12-07 21:43:31 +0800

    feat: button component

---
+++ src/components/button/index.tsx
@@ -0,0 +1,46 @@
+import { Button as AntdButton, Space } from 'antd'
+import type { ButtonProps as AntdButtonProps } from 'antd'
+import { compact } from 'lodash-es'
+import type { ReactNode } from 'react'
+
+import Icon from '@/components/icon'
+
+export type ButtonProps = AntdButtonProps & {
+  icon?: string
+  iconRight?: boolean
+}
+
+const Button = (props: ButtonProps) => {
+  const { icon, iconRight, children, ...attrs } = props
+  const antdButtonProps = {
+    ...attrs
+  }
+  const spaceWrapper = (children: ReactNode[]) => {
+    let space = 0
+    const size = attrs.size || 'middle'
+    switch (size) {
+      case 'small':
+        space = 4
+        break
+      case 'large':
+        space = 12
+        break
+      default:
+        space = 8
+        break
+    }
+    if (children.length > 1) {
+      return <Space size={space}>{children}</Space>
+    }
+    return children
+  }
+
+  const buttonIcon = icon ? <Icon icon={icon} /> : null
+  const content = compact([buttonIcon, children])
+  if (iconRight) {
+    content.reverse()
+  }
+  return <AntdButton {...antdButtonProps}>{spaceWrapper(content)}</AntdButton>
+}
+
+export default Button
