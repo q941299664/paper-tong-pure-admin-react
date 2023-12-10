559072268b70a1710cf9d7cbc63daee4cad1ab0b
sunhx <673686754@qq.com>
2023-12-10 21:28:29 +0800

    feat: add props to action-create component

--- src/components/action/create/index.tsx
+++ src/components/action/create/index.tsx
@@ -1,7 +1,14 @@
 import ActionCore from '../core'
+import type { ActionCoreProps } from '../core'
 
-const ActionCreate = () => {
-  return <ActionCore icon="icon-park-outline:add-one">新建</ActionCore>
+export interface ActionCreateProps extends ActionCoreProps {}
+
+const ActionCreate = (props: ActionCreateProps) => {
+  return (
+    <ActionCore icon="icon-park-outline:add-one" {...props}>
+      新建
+    </ActionCore>
+  )
 }
 
 export default ActionCreate
