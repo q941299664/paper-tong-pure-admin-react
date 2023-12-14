36f32b964e07244da7e334e3590562147629f58d
sunhx <673686754@qq.com>
2023-12-14 15:12:29 +0800

    feat: add DividerGroup component

---
+++ src/components/divider-group/index.tsx
@@ -0,0 +1,26 @@
+import { Divider } from 'antd'
+import { Children, Fragment } from 'react'
+import type { ReactNode } from 'react'
+
+import { joinItem } from '@/utils'
+
+interface DividerGroupProps {
+  children: ReactNode
+}
+
+const DividerGroup = (props: DividerGroupProps) => {
+  const { children } = props
+  const divider = <Divider type="vertical" />
+  const childList = Children.toArray(children)
+
+  const result = joinItem(childList, divider)
+  return (
+    <>
+      {result.map((item, index) => {
+        return <Fragment key={index}>{item}</Fragment>
+      })}
+    </>
+  )
+}
+
+export default DividerGroup
